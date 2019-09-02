cabf179bfece35d86be5d99d7f6809a2f73068f5
Jeffrey Yau <jeffreyyau@outlook.com>
2019-09-02 20:30:00 +0800

    feat: Add a png based clock and media query

--- js/script.js
+++ js/script.js
@@ -16,8 +16,38 @@ function updateTime() {
     let n_date = nt.getDate();
     let n_day = nt.getDay();
 
+    /* pic-clk start */
+    //時分秒的個位數 by pic-clk
+    let n_hx = n_hour % 10;
+    let n_mx = n_min % 10;
+    let n_sx = n_sec % 10;
+
+    //時分秒的十位數 by pic-clk
+    let n_ht = Math.floor(n_hour / 10);
+    let n_mt = Math.floor(n_min / 10);
+    let n_st = Math.floor(n_sec / 10);
+
+    document.getElementById("ck1").className = "c" + n_ht;
+    document.getElementById("ck2").className = "c" + n_hx;
+    document.getElementById("ck3").className = "c" + n_mt;
+    document.getElementById("ck4").className = "c" + n_mx;
+    document.getElementById("ck5").className = "c" + n_st;
+    document.getElementById("ck6").className = "c" + n_sx;
+
+    document.getElementsByClassName("cc")[0].style.opacity = 1;
+    document.getElementsByClassName("cc")[1].style.opacity = 1;
+
+    //兩行DOM延遲500毫秒執行
+    setTimeout(function () {
+    document.getElementsByClassName("cc")[0].style.opacity = 0;
+    document.getElementsByClassName("cc")[1].style.opacity = 0;
+    }, 500);
+    /* pic-clk end */
+
+    /* digi-clk start */
     document.getElementById("time").textContent = `${check(n_hour)}:${check(n_min)}:${check(n_sec)}`;
     document.getElementById("date").textContent = `${n_year}-${check(n_moth + 1)}-${check(n_date)} ${dotw[n_day]}`;
+    /* digi-clk end */
 }
 
 updateTime();
